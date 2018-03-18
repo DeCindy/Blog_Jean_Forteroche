d7bd8663f9580e4be5c0cc3b80500808dc3968b1
CindyD <derrien.cindy@gmail.com>
2018-03-18 17:13:46 +0100

    Ajouté icone de notif menu admin si signalement

--- view/backend/template.php
+++ view/backend/template.php
@@ -2,6 +2,7 @@
 <html>
     <head>
         <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, user-scalable=no">
         <title><?= $title ?></title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.css">
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
@@ -13,18 +14,39 @@
     <body>
         <nav>
             <div class="nav-wrapper black">
-                  <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
-                  <ul class="right hide-on-med-and-down">
+                <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+                <ul class="right hide-on-med-and-down">
+                    <?php
+                    $commentManager = new Blog\Model\CommentManager();
+                    $test = $commentManager->testReport();
+                    if(!empty($test))
+                    {
+                    ?>
+                        <li><a href="index.php?action=manageComments"><i class="material-icons red-text">warning</i></a></li>
+                    <?php
+                    }
+                    ?>
                     <li><a href="index.php?action=adminView">Accueil</a></li>
                     <li><a href="index.php?action=editChapter">Gérer les chapitres</a></li>
                     <li><a href="index.php?action=manageComments">Gérer les commentaires</a></li>
                     <li><a href="index.php?action=home">Retour au site</a></li>
-                  </ul>
-                  <ul class="side-nav" id="mobile-demo">
-                    <li><a href="#">Creer un nouveau chapitre</a></li>
-                    <li><a href="#">Gérer les commentaires</a></li>
+                </ul>
+                <ul class="side-nav" id="mobile-demo">
+                  <?php
+                    $commentManager = new Blog\Model\CommentManager();
+                    $test = $commentManager->testReport();
+                    if(!empty($test))
+                    {
+                    ?>
+                        <li><a href="index.php?action=manageComments"><i class="material-icons red-text">warning</i></a></li>
+                    <?php
+                    }
+                    ?>
+                    <li><a href="index.php?action=adminView">Accueil</a></li>
+                    <li><a href="index.php?action=editChapter">Creer un nouveau chapitre</a></li>
+                    <li><a href="index.php?action=manageComments">Gérer les commentaires</a></li>
                     <li><a href="index.php?action=home">Retour au site</a></li>
-                  </ul>
+                </ul>
             </div>
       </nav>
 
--- view/frontend/template.php
+++ view/frontend/template.php
@@ -2,6 +2,7 @@
 <html>
     <head>
         <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, user-scalable=no">
         <title><?= $title ?></title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.css">
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
